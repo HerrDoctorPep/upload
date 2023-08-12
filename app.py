23a1f08f1acd3aa2d127e8870b49aa1560919041
HerrDoctorPep <github@vdlaan.eu>
2023-08-12 20:25:34 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import uuid
+from flask import Flask, render_template, request
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+app = Flask(__name__)
+
+# Configuration
+account_name = "s2torage"
+account_key = os.environ["STORAGE_KEY"] # In deployment make sure the variable exists
+container_name = "s2t"
+
+# Initialize Blob Service Client
+blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
+container_client = blob_service_client.get_container_client(container_name)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file part"
+
+        file = request.files["file"]
+        if file.filename == "":
+            return "No selected file"
+
+        # Generate a unique filename
+        unique_filename = str(uuid.uuid4()) + "-" + file.filename
+        blob_client = container_client.get_blob_client(unique_filename)
+
+        # Upload the file to Azure Blob Storage
+        blob_client.upload_blob(file)
+
+        return "File uploaded successfully"
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Readme
+
+## Project description
+
+Little git repository to test file upload.
+
+Objectives:
+- Prove small Docker image with Python
+- Deploy code to Azure web app
+- Connect docker container as local storage when deployed
+
+## Learnings
+
+### Docker image
+
+- I let go of Conda in favour of `venv` to get a much smaller docker image(too much weight in conda)
+- Started from `ubuntu:latest` image with a few additional `apt-get install` things.
+- Needed to install `python3`, `python3-venv`, and `python3-pip` separately (first installing `software-properties-common` and `gpg-agent` to allow use of different repo
+as mentioned [here](https://github.com/dbt-labs/dbt-core/issues/7352))
+- Decided to roll back `openssl` to version 1.1.1 anticipating use of Azure AI (cf. speech2text error correspondence)
+
+As long as I develop in the devcontainer, I don´t need to worry about a `venv`.
+If I wantto develop locally, it is necessary to separate things.
+
+At some point I want to make a requirements file for `apt-get` as well, like [here](https://www.monolune.com/articles/installing-apt-packages-from-a-requirements-file/); to improve maintainablity.
+
+```bash
+sed 's/#.*//' config/requirements.system | xargs sudo apt-get install
+```
+
